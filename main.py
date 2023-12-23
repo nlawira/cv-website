6472179e63b5742ee23281e5a32b7befd12de4ab
nlawira <nathanlawira@gmail.com>
2023-12-23 21:27:50 +0700

    Added more stuff

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ from PIL import Image
 import plotly.express as px
 import pandas as pd
 
-def home():
-    st.header("Hello World")
+class Website():
+    def home():
+        #Main Page
+        st.title("This is a website title")
+        st.header("This is a header")
+        st.subheader("This is a subheader")
+        return
 
-def main():
-    st.sidebar.write("I am a sidebar")
-    page = st.sidebar.selectbox('Select Page', ["Home", "Work"], index=0)
-    if page == "Home":
+    def work():
+        #Main Page
+        return
+
+    def main():
+        #Main Page
         home()
+        
+        #Sidebar
+        st.sidebar.header("This is a sidebar header")
+        st.sidebar.image("C:\Users\nathanlawira\Personal\Images\Personal_Photo.jpg")
+        st.sidebar.write("This is a sidebar")
+        page = st.sidebar.selectbox('Select Page', ["Home", "Work"], index=0)
+        if page == "Home":
+            home()
 
 if __name__ == "__main__":
-    main()
+    Website.main()
