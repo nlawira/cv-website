052dfba3555c663c5556a9ed66655711879879a6
nlawira <nathanlawira@gmail.com>
2023-12-25 21:43:23 +0700

    Added colors

--- main.py
+++ main.py
@@ -7,23 +7,55 @@ from PIL import Image
 import plotly.express as px
 import pandas as pd
 
-#Home Page
+# Initial Settings
+st.set_page_config(page_title="My Webpage", layout="wide")
+
+# Use Local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style/", unsafe_allow_html=True)
+
+local_css("style/style.css")
+
+# Defining Pages
 def home():
-    #Title
-    st.title("Hello, I am Nathan! :wave:")
-    
-    #Header Section
-    st.subheader("Summary")
-    st.write("I am a Chemical & Biomolecular Engineering student at Nanyang Technological University and will graduate in June 2024. \
-             My academic background equips me with strong analytical, problem-solving, and \
-             critical thinking skills, while my enthusiasm and passion for data analytics, coding, and software development \
-             reflects my keen interest in leveraging technologies to address and solve complex challenges!")
-    st.write("Eager to apply my skills, I am actively seeking a full-time position in data analytics, coding, or software-related \
-             fields where I can provide my expertise to create lasting contributions and continuously improve and grow professionally!")
-    st.write("---")
+    with st.container():
+        # --- TITLE & SUMMARY ---
+        st.title("Hello, I am Nathan! :wave:")
+        st.subheader("Summary")
+        st.write("I am a Chemical & Biomolecular Engineering student at Nanyang Technological University and will graduate in June 2024. \
+                My academic background equips me with strong analytical, problem-solving, and \
+                critical thinking skills, while my enthusiasm and passion for data analytics, coding, and software development \
+                reflects my keen interest in leveraging technologies to address and solve complex challenges!")
+        st.write("Eager to apply my skills, I am actively seeking a full-time position in data analytics, coding, or software-related \
+                fields where I can provide my expertise to create lasting contributions and continuously improve and grow professionally!")
+    with st.container():
+        # ---
+        st.write("---")
+    with st.container():
+        # --- PERSONALITY ---
+        st.write("---")
+        st.subheader("Personality")
+    with st.container():
+        # --- CONTACT ME ---
+        st.write("---")
+        st.subheader("Get in Touch with Me!")
+        contact_form = """
+        <form action="https://formsubmit.co/nathanlawira@gmail.com" method="POST">
+            <input type="hidden" name="_captcha" value="false">
+            <input type="text" name="name" placeholder="Your Name" required>
+            <input type="email" name="email" placeholder="Your Email" required>
+            <textarea name="message" placeholder="Your Message Here" required></textarea>
+            <button type="submit">Send</button>
+        </form>
+        """
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.markdown(contact_form, unsafe_allow_html=True)
+        with right_column:
+            st.empty()
     return
 
-#Main Page
 def work():
     st.title("This is the work page")
     return
@@ -41,33 +73,29 @@ def hobbies():
     return
 
 def main():
-    #Default settings
-    st.set_page_config(page_title="My Webpage", layout="wide")
+    with st.container():
+        # Sidebar Image and Description
+        st.sidebar.image(Image.open("Images/Personal_Photo.jpg"), use_column_width = True)
+        st.sidebar.markdown("""
+        <center>
+            <h1>Nathan Lawira</h1>
+            <p>Senior Undergraduate Student in <b>Nanyang Technological University</b></p>
+            <p>Pursuing a <b>Bachelor of Engineering in Chemical & Biomolecular Engineering</b> Specializing in Intellectual Property with a Minor in Modern Languages</p>
+        </center>
+        """, unsafe_allow_html=True)
 
-    #Sidebar
-    st.sidebar.image(Image.open("Images/Personal_Photo.jpg"), use_column_width = True)
-    st.sidebar.markdown("""
-    <center>
-        <h1>Nathan Lawira</h1>
-        <p>Senior Undergraduate Student in <b>Nanyang Technological University</b></p>
-        <p>Pursuing a <b>Bachelor of Engineering in Chemical & Biomolecular Engineering</b> Specializing in Intellectual Property with a Minor in Modern Languages</p>
-    </center>
-    """, unsafe_allow_html=True)
+        # Social Media Links
+        st.sidebar.markdown("""
+        <div style="display: flex; justify-content: center;">
+            <a href='mailto:nathanlawira@gmail.com'><img src='https://static.vecteezy.com/system/resources/previews/020/009/614/original/email-and-mail-icon-black-free-png.png' width='30' style='margin-right: 20px;'></a>
+            <a href='https://www.linkedin.com/in/nathan-lawira/'><img src='https://img.icons8.com/color/48/000000/linkedin.png' width='30' style='margin-right: 20px;'></a>
+            <a href='https://github.com/nlawira'><img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' width='30' style='margin-right: 20px;'></a>            
+            <a href='https://www.instagram.com/nlawira/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png' width='30'></a>
+        </div>
+        """, unsafe_allow_html=True)
 
-    #Social Media Links
-    st.sidebar.markdown("""
-    <div style="display: flex; justify-content: center;">
-        <a href='mailto:nathanlawira@gmail.com'><img src='https://static.vecteezy.com/system/resources/previews/020/009/614/original/email-and-mail-icon-black-free-png.png' width='30' style='margin-right: 20px;'></a>
-        <a href='https://www.linkedin.com/in/nathan-lawira/'><img src='https://img.icons8.com/color/48/000000/linkedin.png' width='30' style='margin-right: 20px;'></a>
-        <a href='https://github.com/nlawira'><img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' width='30' style='margin-right: 20px;'></a>            
-        <a href='https://www.instagram.com/nlawira/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png' width='30'></a>
-    </div>
-    """, unsafe_allow_html=True)
-
-    # Rest of your Streamlit app code
+    # Page Selection
     st.sidebar.write("---")
-
-    #Main Page Selection
     page = st.sidebar.selectbox('Select Page', ["Home", "Work","Education","Co-Curricular & Voluntary Activies","Hobbies & Personal Life"], index=0)
     if page == "Home":
         home()
