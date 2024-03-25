d22562e6fde28c91cd40b18bfef69ed0a77ce59c
Nathan <nathanlawira@gmail.com>
2024-03-25 22:24:52 +0800

    Added a new cert and editted the education tab

--- main.py
+++ main.py
@@ -617,53 +617,54 @@ def education():
         st.write("Topic: **Article 222 Law No. 7 of 2017 Governing Elections (Presidential Threshold)**")
         st.caption("**Aug 2018 -  May 2019**")
         st.caption("Associated with **Cahaya Bangsa Classical School**")
-        st.caption("Delivered in **Bahasa Indonesia**")
-        st.write("This part is still a work-in-progress. Sorry!")
+        st.caption("Written and Presented in **Bahasa Indonesia**")
+        st.write("- **Conducted a comprehensive analysis** of Article 222 within Law No. 7 of 2017, which pertains to the regulations surrounding the presidential threshold in Indonesian elections\n",
+                 "- Reviewed historical election data and legal documents to understand the evolution and implementation of the presidential threshold since its inception\n",
+                 "- **Engaged in critical discourse analysis** to identify and discuss the implications of the presidential threshold on political party strategies and voter behavior")
     st.write("- - -")
     
-    # Relevant Classes/Modules
-    st.subheader("Relevant Classes/Modules")
-    st.write("")
+    # # Relevant Classes/Modules
+    # st.subheader("Relevant Classes/Modules")
+    # st.write("")
 
-    #CB4247
-    with st.container():
-        st.subheader("CB4247 Statistics & Computational Inference to Big Data")
-        left,right=st.columns([1.5,1])
-        with left:
-            st.caption("**2023-2024 Semester 2**")
-            st.caption("Associated with **Nanyang Technological University**")
-            st.write("This part is still a work-in-progress. Sorry!")
-        with right:
-            st.write("")
+    # #CB4247
+    # with st.container():
+    #     st.subheader("CB4247 Statistics & Computational Inference to Big Data")
+    #     left,right=st.columns([1.5,1])
+    #     with left:
+    #         st.caption("**2023-2024 Semester 2**")
+    #         st.caption("Associated with **Nanyang Technological University**")
+    #         st.write("This part is still a work-in-progress. Sorry!")
+    #     with right:
+    #         st.write("")
     
-    # Space Divider
-    st.write("")
-    st.write("")
-    st.write("")
+    # # Space Divider
+    # st.write("")
+    # st.write("")
+    # st.write("")
 
-    #CB0494
-    with st.container():
-        st.subheader("CB0494 Introduction to Data Science and Artificial Intelligence")
-        left,right=st.columns([1,1.5])
-        with left:
-            st.write("")
-        with right:
-            st.caption("**2021-2022 Semester 2**")
-            st.caption("Associated with **Nanyang Technological University**")
-            st.write("This part is still a work-in-progress. Sorry!")
+    # #CB0494
+    # with st.container():
+    #     st.subheader("CB0494 Introduction to Data Science and Artificial Intelligence")
+    #     left,right=st.columns([1,1.5])
+    #     with left:
+    #         st.write("")
+    #     with right:
+    #         st.caption("**2021-2022 Semester 2**")
+    #         st.caption("Associated with **Nanyang Technological University**")
+    #         st.write("This part is still a work-in-progress. Sorry!")
 
-    #CH2010
-    with st.container():
-        st.subheader("CH2010 Engineering Statistics")
-        left,right=st.columns([1.5,1])
-        with left:
-            st.caption("**2021-2022 Semester 1**")
-            st.caption("Associated with **Nanyang Technological University**")
-            st.write("This part is still a work-in-progress. Sorry!")
-        with right:
-            st.write("")
-
-    st.write("- - -")
+    # #CH2010
+    # with st.container():
+    #     st.subheader("CH2010 Engineering Statistics")
+    #     left,right=st.columns([1.5,1])
+    #     with left:
+    #         st.caption("**2021-2022 Semester 1**")
+    #         st.caption("Associated with **Nanyang Technological University**")
+    #         st.write("This part is still a work-in-progress. Sorry!")
+    #     with right:
+    #         st.write("")
+    # st.write("- - -")
     
     # Online Courses
     st.subheader("Online Courses")
