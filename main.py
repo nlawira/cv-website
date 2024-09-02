6e8e5d6b387695d96263f9b01561a89578829b20
Nathan <nathanlawira@gmail.com>
2024-09-02 13:14:06 +0800

    Updated Dean's List award (font size & by whom)

--- main.py
+++ main.py
@@ -171,10 +171,11 @@ def home():
         # --- AWARDS & HONORS ---
         st.write("---")
         st.subheader("Awards & Honors")
-        st.write("**Dean's List** for Academic Year 2023-2024")
-        st.caption("Awarded on **June 2024**")
-        st.caption("Associated with **Nanyang Technological University**")
-        st.write("- Granted to students in the **top 5% of the cohort** for academic excellence, subject to attaining a minimum YGPA of 4.50 and taking at least 16 AU of graded courses in the academic year")
+        with st.container():
+            st.subheader("**Dean's List** for Academic Year 2023-2024")
+            st.caption("Awarded on **June 2024** by **Dean, College of Engineering**")
+            st.caption("Associated with **Nanyang Technological University**")
+            st.write("- Granted to students in the **top 5% of the cohort** for academic excellence, subject to attaining a minimum YGPA of 4.50 and taking at least 16 AU of graded courses in the academic year")
 
     with st.container():
         # --- WORK EXPERIENCE ---
